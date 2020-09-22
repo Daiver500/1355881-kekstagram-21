3409d7fb62ff24bde5011c2528a46983510aef68
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-09-23 00:56:18 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,176 @@
+"use strict";
+
+const photosDescription = [
+  {
+    url: `photos/1.jpg`,
+    description: `Фото`,
+    likes: 1,
+    comments:
+  },
+
+  {
+    url: `photos/2.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+
+  {
+    url: `photos/3.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+
+  {
+    url: `photos/4.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+
+  {
+    url: `photos/5.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+
+  {
+    url: `photos/6.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+
+  {
+    url: `photos/7.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/8.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/9.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/10.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/11.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/12.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/13.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/14.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/15.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/16.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/17.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/18.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/19.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/20.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/21.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/22.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/23.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/24.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+  {
+    url: `photos/25.jpg`,
+    description: `Фото`,
+    likes: 2,
+    comments:
+  },
+];
+
+
+const comments = [
+  {
+    avatar: `img/avatar-1.svg`,
+    message: `В целом всё неплохо. Но не всё.`,
+    name: `Артем`
+  },
+  {
+    avatar: `img/avatar-6.svg`,
+    message: `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+    name: `Вася`
+  }
+];
+
+const likes = Math.round(Math.random()*100);
