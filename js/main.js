461ec5c6836c6c9b3529525098bcef082239aec8
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-09-23 23:28:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,13 +9,11 @@ const creationDescription = function () {
   }
 };
 
-// массив объектов — список комментариев
+const randomReview = Math.floor(Math.random() * reviews.length);
 
-const creationReplies = function () {
+const creationReviews = function () {
   let replies = [];
-  for (let review of reviews) {
-    replies.push(review[0] || review[1] || review[2] || review[3] || review[4] || review[5] || review[6]);
-  }
+  replies.push(randomReview);
 };
 
 // Случайное число от 15 до 200 (для лайков)
@@ -32,177 +30,227 @@ const photos = [
     url: `photos/1.jpg`, // строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} это число от 1 до 25. Адреса картинок не должны повторяться.
     description: `Фото`, // строка — описание фотографии.
     likes: getRandom(15, 200), // число — количество лайков, поставленных фотографии. Случайное число от 15 до 200
-    comments: creationReplies, // массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом.
+    comments: creationReviews, // массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии вы определяете на своё усмотрение. Все комментарии генерируются случайным образом.
   },
 
   {
     url: `photos/2.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/3.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/4.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/5.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/6.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/7.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/8.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/9.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/10.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/11.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/12.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/13.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/14.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/15.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/16.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/17.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/18.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/19.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/20.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/21.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/22.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/23.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/24.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 
   {
     url: `photos/25.jpg`,
     description: `Фото`,
     likes: getRandom(15, 200),
-    comments: creationReplies,
+    comments: creationReviews,
   },
 ];
+
+// Массив сообщений
+
+const messages = [
+  {
+    message: `В целом всё неплохо. Но не всё.`
+  },
+  {
+    message: `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`
+  }
+];
+
+// Рандомное сообщение
+
+const randomMessage = Math.floor(Math.random() * messages.length);
+
+const creationMessages = function () {
+  reviews.name = randomMessage;
+};
+
+// Массив имен
+
+const names = [
+  {
+    name: `Артем`
+  },
+  {
+    name: `Петя`
+  },
+  {
+    name: `Вася`
+  },
+  {
+    name: `Даша`
+  },
+  {
+    name: `Иван`
+  },
+  {
+    name: `Алексей`
+  }
+];
+
+// Рандомное имя
+
+const randomName = Math.floor(Math.random() * names.length);
+
+const creationNames = function () {
+  reviews.name = randomName;
+};
 
 // Массив объектов
 
@@ -234,42 +282,8 @@ const reviews = [
   },
   {
     avatar: `img/avatar-6.svg`,
-    message: `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-    name: `Алексей`,
-  }
-];
-
-// Массив имен
-
-const names = [
-  {
-    name: `Артем`
-  },
-  {
-    name: `Петя`
-  },
-  {
-    name: `Вася`
-  },
-  {
-    name: `Даша`
-  },
-  {
-    name: `Иван`
-  },
-  {
-    name: `Алексей`
-  }
-];
-
-// Массив сообщений
-
-const messages = [
-  {
-    message: `В целом всё неплохо. Но не всё.`
-  },
-  {
-    message: `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`
+    message: creationMessages,
+    name: creationNames,
   }
 ];
 
