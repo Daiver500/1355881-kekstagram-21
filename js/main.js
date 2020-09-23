67bf211587978d6064bb6d09b219aabde7b19ce8
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-09-23 10:59:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,176 +1,32 @@
 "use strict";
 
-const photosDescription = [
-  {
-    url: `photos/1.jpg`,
-    description: `Фото`,
-    likes: 1,
-    comments:
-  },
-
-  {
-    url: `photos/2.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-
-  {
-    url: `photos/3.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-
-  {
-    url: `photos/4.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-
-  {
-    url: `photos/5.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-
-  {
-    url: `photos/6.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-
-  {
-    url: `photos/7.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/8.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/9.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/10.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/11.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/12.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/13.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/14.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/15.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/16.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/17.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/18.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/19.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/20.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/21.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/22.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/23.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/24.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-  {
-    url: `photos/25.jpg`,
-    description: `Фото`,
-    likes: 2,
-    comments:
-  },
-];
+const names = [`Артем`, `Петя`, `Вася`, `Даша`, `Иван`, `Алексей`];
+const messages = [`В целом всё неплохо. Но не всё.`, `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`];
+const avatars = [`img/avatar-1.svg`, `img/avatar-2.svg`, `img/avatar-3.svg`, `img/avatar-4.svg`, `img/avatar-5.svg`, `img/avatar-6.svg`];
+const photos = [`photos/1.jpg`, `photos/2.jpg`, `photos/3.jpg`, `photos/4.jpg`, `photos/5.jpg`, `photos/6.jpg`, `photos/7.jpg`, `photos/8.jpg`, `photos/9.jpg`, `photos/10.jpg`,
+  `photos/11.jpg`, `photos/12.jpg`, `photos/13.jpg`, `photos/14.jpg`, `photos/15.jpg`, `photos/16.jpg`, `photos/17.jpg`, `photos/18.jpg`, `photos/19.jpg`,
+  `photos/20.jpg`, `photos/21.jpg`, `photos/22.jpg`, `photos/23.jpg`, `photos/24.jpg`, `photos/25.jpg`];
 
 
-const comments = [
+const reviews = [
   {
-    avatar: `img/avatar-1.svg`,
-    message: `В целом всё неплохо. Но не всё.`,
-    name: `Артем`
-  },
-  {
-    avatar: `img/avatar-6.svg`,
-    message: `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-    name: `Вася`
+    avatar: avatars,
+    message: messages,
+    name: names
   }
 ];
 
-const likes = Math.round(Math.random()*100);
+const getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const photosDescription = [
+  {
+    url: photos,
+    description: `Фото`,
+    likes: getRandomNumber(15, 200),
+    comments: reviews
+  }
+];
+
+
