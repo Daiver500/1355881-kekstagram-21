45c7d1a1bc314b4adca48a6c763664a9c389f877
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-10-15 23:07:31 +0300

    Разделяю main.js на модули
    Разделил файл на 6 модулей

---
+++ js/photocorrection.js
@@ -0,0 +1,118 @@
+"use strict";
+
+// Масштаб:
+
+const scaleControlSmaller = document.querySelector(`.scale__control--smaller`);
+const scaleControlBigger = document.querySelector(`.scale__control--bigger`);
+const scaleValue = document.querySelector(`.scale__control--value`);
+const imageUploadPreview = document.querySelector(`.img-upload__preview img`);
+
+const value = {
+  min: 25,
+  max: 100
+};
+
+const onMinusScaleClick = function () {
+  let scale = parseInt(scaleValue.value, 10);
+  if (scale <= value.max && scale > value.min) {
+    scale -= value.min;
+  }
+  imageStyleChange(scale);
+};
+
+scaleControlSmaller.addEventListener(`click`, onMinusScaleClick);
+
+const onPlusScaleClick = function () {
+  let scale = parseInt(scaleValue.value, 10);
+  if (scale >= value.min && scale < value.max) {
+    scale += value.min;
+  }
+  imageStyleChange(scale);
+};
+
+scaleControlBigger.addEventListener(`click`, onPlusScaleClick);
+
+const imageStyleChange = function (number) {
+  switch (number) {
+    case 25:
+      imageUploadPreview.style.transform = `scale(0.25)`;
+      scaleValue.value = `${number}%`;
+      break;
+    case 50:
+      imageUploadPreview.style.transform = `scale(0.50)`;
+      scaleValue.value = `${number}%`;
+      break;
+    case 75:
+      imageUploadPreview.style.transform = `scale(0.75)`;
+      scaleValue.value = `${number}%`;
+      break;
+    case 100:
+      imageUploadPreview.style.transform = `scale(1.00)`;
+      scaleValue.value = `${number}%`;
+      break;
+  }
+};
+
+// Эффект на изображение
+
+const img = document.querySelector(`.img-upload__preview img`);
+const effects = document.querySelector(`.effects`);
+
+const filterChange = function (evt) {
+  if (evt.target.matches(`input[type="radio"]`)) {
+    img.className = ``;
+    img.className = `effects__preview--${evt.target.value}`;
+  }
+};
+
+effects.addEventListener(`click`, filterChange);
+
+
+// Интенсивность эффекта !!!
+
+const effectLevelPin = document.querySelector(`.effect-level__pin`);
+effectLevelPin.addEventListener(`mouseup`, function () {
+});
+const effectLevelValue = document.querySelector(`.effect-level__value`);
+
+effectLevelValue.value = {
+  min: 0,
+  max: 100
+};
+
+effectLevelValue.value  // меняется по перемещению effectLevelPin;
+img.style.filter  // меняется по изменению effectLevelValue.value ;
+
+// В зависимости от примененного класса применяется фильтр
+
+effectLevelValue.addEventListener(`change`, function () {
+  if (img.className === `effects__preview--chrome`) {
+    img.style.filter = `filter: grayscale(0..1)`;
+  }
+  if (img.className === `effects__preview--sepia`) {
+    img.style.filter = `filter: sepia(0..1)`;
+  }
+  if (img.className === `effects__preview--marvin`) {
+    img.style.filter = `filter: invert(0..100%)`;
+  }
+  if (img.className === `effects__preview--phobos`) {
+    img.style.filter = `filter: blur(0..3px)`;
+  }
+  if (img.className === `effects__preview--heat`) {
+    img.style.filter = `filter: brightness(1..3)`;
+  }
+});
+
+const liFirst = document.querySelector(`.effects__item:first-child`);
+const Li = document.querySelector(`.effects__item:not(:first-child)`);
+const imgUploadEffectLevel = document.querySelector(`.img-upload__effect-level`);
+
+imgUploadEffectLevel.classList.add(`hidden`);
+
+liFirst.addEventListener(`click`, function () {
+  imgUploadEffectLevel.classList.add(`hidden`);
+});
+
+Li.addEventListener(`click`, function () {
+  imgUploadEffectLevel.classList.remove(`hidden`);
+});
---
+++ js/validation.js
@@ -0,0 +1,115 @@
+"use strict";
+// Валидация поля для хэштегов
+
+// 1.Описать константы
+
+const HASHTAGS_LENGTH = {
+  min: 2,
+  max: 20
+};
+
+const hashTagsInput = document.querySelector(`.text__hashtags`);
+const pattern = /^([#]{1})([0-9a-zа-яё]{1,19})$/;
+// /^([#]{1})([0-9a-zа-яё]{1,19})$/;
+
+const hashTagsMax = 5;
+
+// 2.Написать функцию получения хэштегов
+const createHashTagsArray = function (hashTagsString) {
+  return hashTagsString.split(` `);
+};
+
+// 3.Написать функцию уборки  пробелов в новом массиве
+const createNewHashtagsArrayWithoutSpaces = function (allHashtags) {
+  const tags = allHashtags.filter((hashtag) => {
+    return hashtag !== ``;
+  });
+  return tags;
+};
+
+// 4.Функция валидации
+const doValidationOfHashtags = function (arrayOfHashtags) {
+  console.log(arrayOfHashtags);
+  arrayOfHashtags.forEach((item, index) => { // проверяем forEach каждый элемент "чистого массива"
+    const valueLength = item.length;
+    console.log(arrayOfHashtags.indexOf(item, index + 1) !== -1);
+    if (!item.startsWith(`#`)) { // проверяем начало хэштега с #
+      hashTagsInput.setCustomValidity(`Нет 3`);
+    } else if (valueLength < HASHTAGS_LENGTH.min) { // проверяем на min значение
+      hashTagsInput.setCustomValidity(`Нет 4`);
+    } else if (valueLength > HASHTAGS_LENGTH.max) { // проверяем на max значение
+      hashTagsInput.setCustomValidity(`Нет 5`);
+    } else if (!item.match(pattern)) {
+      hashTagsInput.setCustomValidity(`Нет 2`);
+    } else if (arrayOfHashtags.length > hashTagsMax) {
+      hashTagsInput.setCustomValidity(`Нет 1`);
+    // } else if (arrayOfHashtags.indexOf(item, index + 1) !== -1) { // проверяем на одинаковые элементы
+      // hashTagsInput.setCustomValidity(`Нет 6`);
+    } else {
+      hashTagsInput.setCustomValidity(``);
+    }
+    hashTagsInput.reportValidity();
+  });
+  for (let i = 0; i < arrayOfHashtags.length; i++) {
+    if (arrayOfHashtags[i] === arrayOfHashtags[i + 1]) {
+      hashTagsInput.setCustomValidity(`Нет 6`);
+    } else if (arrayOfHashtags[i] === arrayOfHashtags[i + 2]) {
+      hashTagsInput.setCustomValidity(`Нет 7`);
+    } else if (arrayOfHashtags[i] === arrayOfHashtags[i + 3]) {
+      hashTagsInput.setCustomValidity(`Нет 8`);
+    } else if (arrayOfHashtags[i] === arrayOfHashtags[i + 4]) {
+      hashTagsInput.setCustomValidity(`Нет 9`);
+    }
+    hashTagsInput.reportValidity();
+  }
+};
+
+
+// 5.Функция обработчик
+
+const hashTagsInputKeyupHandler = function () {
+  const inputValue = hashTagsInput.value.trim().toLowerCase();
+  const dirtyHashTags = createHashTagsArray(inputValue);
+  const cleanHashTags = createNewHashtagsArrayWithoutSpaces(dirtyHashTags);
+  doValidationOfHashtags(cleanHashTags);
+
+  if (!hashTagsInput.validity.valid) {
+    hashTagsInput.style.outline = `2px solid red`;
+  } else {
+    hashTagsInput.style.outline = `none`;
+  }
+};
+
+hashTagsInput.addEventListener(`keyup`, hashTagsInputKeyupHandler);
+
+hashTagsInput.addEventListener(`focusin`, function () {
+  document.removeEventListener(`keydown`, modalEscPress);
+});
+
+hashTagsInput.addEventListener(`focusout`, function () {
+  document.addEventListener(`keydown`, modalEscPress);
+});
+
+// Поле ввода комментария
+
+const commentsField = document.querySelector(`.text__description`);
+const COMMENTS_MAX = 120;
+
+commentsField.oninput = function () {
+  const valueLength = commentsField.value.length;
+  if (commentsField.value.length > COMMENTS_MAX) {
+    commentsField.setCustomValidity(`Удалите ` + (COMMENTS_MAX - valueLength) + ` симв.`);
+  } else {
+    commentsField.setCustomValidity(``);
+  }
+  commentsField.reportValidity();
+};
+
+commentsField.addEventListener(`focusin`, function () {
+  document.removeEventListener(`keydown`, modalEscPress);
+});
+
+commentsField.addEventListener(`focusout`, function () {
+  document.addEventListener(`keydown`, modalEscPress);
+});
+
