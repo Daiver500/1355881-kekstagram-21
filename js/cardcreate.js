d87434cf5884c1325a3073fe4e35a2fc3193c540
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-10-20 21:24:52 +0300

    Заменил моки на данные

--- js/cardcreate.js
+++ js/cardcreate.js
@@ -23,118 +23,134 @@
     return cardElement;
   };
 
-  // Добавление элемента через documentFragment
+  // 6.2
+  const successHandler = function (cards) {
+    const renderPictures = function () {
+      const pictures = document.querySelector(`.pictures`);
+      const fragment = document.createDocumentFragment();
+      cards.forEach(function (value) {
+        fragment.appendChild(createCardElement(value));
+      });
+      pictures.appendChild(fragment);
+      return fragment;
+    };
 
-  const renderPictures = function () {
-    const pictures = document.querySelector(`.pictures`);
-    const fragment = document.createDocumentFragment();
-    window.mockscreation.mocks.forEach(function (value) {
-      fragment.appendChild(createCardElement(value));
+    renderPictures(cards);
+
+    const createSocialComment = function (object) {
+      const fragment = document.createDocumentFragment();
+      const socialComments = document.querySelector(`.social__comments`);
+      const socialComment = socialComments.querySelector(`li`);
+      socialComments.innerHTML = ``;
+
+      object.comments.forEach(function (value) {
+        const li = socialComment.cloneNode(true);
+        li.querySelector(`.social__picture`).src = value.avatar;
+        li.querySelector(`.social__picture`).alt = value.name;
+        li.querySelector(`.social__picture`).width = AVATAR.width;
+        li.querySelector(`.social__picture`).height = AVATAR.height;
+        li.querySelector(`.social__text`).textContent = value.message;
+        fragment.append(li);
+        socialComments.append(fragment);
+      });
+    };
+
+    const bigPicture = document.querySelector(`.big-picture`);
+    const bigPictureOpened = function (object) {
+      bigPicture.classList.remove(`hidden`);
+      const {url, likes, comments, description} = object;
+      bigPicture.querySelector(`.big-picture__img img`).src = url;
+      bigPicture.querySelector(`.likes-count`).textContent = likes;
+      bigPicture.querySelector(`.comments-count`).textContent = comments;
+      bigPicture.querySelector(`.social__caption`).textContent = description;
+      // createSocialComment(cards);
+    };
+    bigPictureOpened(cards);
+
+    bigPicture.classList.add(`hidden`);
+
+    // Добавляем класс hidden
+
+    const socialCommentCount = document.querySelector(`.social__comment-count`);
+    socialCommentCount.classList.add(`hidden`);
+
+    const commentsLoader = document.querySelector(`.comments-loader`);
+    commentsLoader.classList.add(`hidden`);
+
+    // Добавляем класс на body (для фиксации фона)
+
+    document.querySelector(`body`).classList.add(`modal-open`);
+
+    // Раздел 4.2.
+
+    const smallPhotos = document.querySelectorAll(`.picture`);
+
+    const addSmallPhotoClicker = function (smallphoto, content) {
+      smallphoto.addEventListener(`click`, function (evt) {
+        evt.preventDefault();
+        document.addEventListener(`keydown`, bigPictureEscPress);
+        bigPictureOpened(content);
+        createSocialComment(content);
+      });
+    };
+
+    for (let i = 0; i < smallPhotos.length; i++) {
+      addSmallPhotoClicker(smallPhotos[i], cards[i]);
+    }
+
+    // Закрываем превью фото с коментами
+
+
+    const bigPictureCancel = document.querySelector(`.big-picture__cancel`);
+
+    const bigPictureEscPress = function (evt) {
+      if (evt.key === `Escape`) {
+        evt.preventDefault();
+        closeBigPicture();
+      }
+    };
+
+    document.addEventListener(`keydown`, bigPictureEscPress);
+
+    const closeBigPicture = function () {
+      bigPicture.classList.add(`hidden`);
+      document.removeEventListener(`keydown`, bigPictureEscPress);
+    };
+
+    bigPictureCancel.addEventListener(`click`, function () {
+      closeBigPicture();
     });
-    pictures.appendChild(fragment);
-    return fragment;
   };
 
-  renderPictures(window.mockscreation.mocks);
 
   // 6.1
+
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  window.load(successHandler, errorHandler);
 
   const form = document.querySelector(`.img-upload__form`);
   const imageUploadOverlay = document.querySelector(`.img-upload__overlay`);
 
-  form.addEventListener(`submit`, function (evt) {
-    window.upload(new FormData(form), function (response) {
+  const submitHandler = function (evt) {
+    window.upload(new FormData(form), function () {
       imageUploadOverlay.classList.add(`hidden`);
-
     });
     window.modalopenclose.uploadImageFile.value = ``;
     window.validation.hashTagsInput.textContent = ``;
     window.validation.commentsField.textContent = ``;
     window.effects.setDefaultDepth();
     evt.preventDefault();
-  });
-
-  const createSocialComment = function (object) {
-    const fragment = document.createDocumentFragment();
-    const socialComments = document.querySelector(`.social__comments`);
-    const socialComment = socialComments.querySelector(`li`);
-    socialComments.innerHTML = ``;
-
-    object.comments.forEach(function (value) {
-      const li = socialComment.cloneNode(true);
-      li.querySelector(`.social__picture`).src = value.avatar;
-      li.querySelector(`.social__picture`).alt = value.name;
-      li.querySelector(`.social__picture`).width = AVATAR.width;
-      li.querySelector(`.social__picture`).height = AVATAR.height;
-      li.querySelector(`.social__text`).textContent = value.message;
-      fragment.append(li);
-      socialComments.append(fragment);
-    });
   };
-
-  const bigPicture = document.querySelector(`.big-picture`);
-  const bigPictureOpened = function (object) {
-    bigPicture.classList.remove(`hidden`);
-    const {url, likes, comments, description} = object;
-    bigPicture.querySelector(`.big-picture__img img`).src = url;
-    bigPicture.querySelector(`.likes-count`).textContent = likes;
-    bigPicture.querySelector(`.comments-count`).textContent = comments.length;
-    bigPicture.querySelector(`.social__caption`).textContent = description;
-    createSocialComment(window.mockscreation.mocks[0]);
-  };
-  bigPictureOpened(window.mockscreation.mocks[0]);
-
-  bigPicture.classList.add(`hidden`);
-
-  // Добавляем класс hidden
-
-  const socialCommentCount = document.querySelector(`.social__comment-count`);
-  socialCommentCount.classList.add(`hidden`);
-
-  const commentsLoader = document.querySelector(`.comments-loader`);
-  commentsLoader.classList.add(`hidden`);
-
-  // Добавляем класс на body (для фиксации фона)
-
-  document.querySelector(`body`).classList.add(`modal-open`);
-
-  // Раздел 4.2.
-
-  const smallPhotos = document.querySelectorAll(`.picture`);
-
-  const addSmallPhotoClicker = function (smallphoto, content) {
-    smallphoto.addEventListener(`click`, function (evt) {
-      evt.preventDefault();
-      document.addEventListener(`keydown`, bigPictureEscPress);
-      bigPictureOpened(content);
-      createSocialComment(content);
-    });
-  };
-
-  for (let i = 0; i < smallPhotos.length; i++) {
-    addSmallPhotoClicker(smallPhotos[i], window.mockscreation.mocks[i]);
-  }
-
-  // Закрываем превью фото с коментами
-
-
-  const bigPictureCancel = document.querySelector(`.big-picture__cancel`);
-
-  const bigPictureEscPress = function (evt) {
-    if (evt.key === `Escape`) {
-      evt.preventDefault();
-      closeBigPicture();
-    }
-  };
-
-  document.addEventListener(`keydown`, bigPictureEscPress);
-
-  const closeBigPicture = function () {
-    bigPicture.classList.add(`hidden`);
-    document.removeEventListener(`keydown`, bigPictureEscPress);
-  };
-
-  bigPictureCancel.addEventListener(`click`, function () {
-    closeBigPicture();
-  });
+  form.addEventListener(`submit`, submitHandler);
 })();
--- js/upload.js
+++ js/upload.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  const URL = `https://21.javascript.pages.academy/kekstagram `;
+  const URL = `https://21.javascript.pages.academy/kekstagram`;
 
   window.upload = function (data, onSuccess) {
     const xhr = new XMLHttpRequest();
