9c234f82e7107779300ea59b20bdecae473bd285
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-10-31 16:52:24 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
+const path = require("path")
+
 module.exports = {
  entry: [
-   "./js/script.js",
-   "./js/*.js"
-]
+   "./js/server.js",
+   "./js/bigpicture.js",
+   "./js/cardcreate.js",
+   "./js/filters.js",
+   "./js/modalopenclose.js",
+   "./js/submit.js",
+   "./js/success.js",
+   "./js/error.js",
+   "./js/validation.js",
+   "./js/effects.js",
+   "./js/scale.js",
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
 };
