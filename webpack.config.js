98f5cf62813c7212b0a99dfc94c81e1d36b9575a
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-11-02 14:32:53 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/server.js",
+    "./js/bigpicture.js",
+    "./js/cardcreate.js",
+    "./js/filters.js",
+    "./js/validation.js",
+    "./js/modalopenclose.js",
+    "./js/submit.js",
+    "./js/success.js",
+    "./js/error.js",
+    "./js/effects.js",
+    "./js/scale.js",
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "finaljs"),
+    iife: true
+  },
+  devtool: false
+}
