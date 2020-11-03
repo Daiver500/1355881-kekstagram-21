44e2e736e0cee8ee8569bd2be53ca045ae780929
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-11-03 17:21:13 +0300

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -1,106 +1,84 @@
 "use strict";
 
 (function () {
-
-  const filterDefaultPhotos = document.querySelector(`#filter-default`);
-  const filterRandomPhotos = document.querySelector(`#filter-random`);
-  const filterMaxCommentsPhotos = document.querySelector(`#filter-discussed`);
-
-  let lastTimeout;
+  const MAX_RANDOM_ELEMENTS = 10;
   const DEBOUNCE_INTERVAL = 500;
-  window.debounce = function (cb) {
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-    lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
+  const filtersForm = document.querySelector(`.img-filter__form`);
+  const renderPictures = window.cardcreate.renderPictures;
+
+  const debounce = (cb) => {
+    let lastTimeout = null;
+    return (...parameters) => {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(() => {
+        cb(...parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
   };
 
-  // Массив фото с сервера (дефолт)
-
-  const photosFromServerDefault = function () {
-    const defaultPhotos = window.cardcreate.cardList;
-    window.cardcreate.renderPictures(defaultPhotos);
-    window.cardcreate.test(defaultPhotos);
+  // показываем дефолтные картинки
+  const showDefaultPictures = function () {
+    renderPictures(window.data.picturesList);
   };
 
-  // Массив 10 рандомных фото
+  // функция перемешивания массива
+  const shuffleArray = function (array) {
+    const arrayCopy = array.slice();
+    const iterations = MAX_RANDOM_ELEMENTS < arrayCopy.length ? MAX_RANDOM_ELEMENTS : arrayCopy.length - 1;
 
-  const tenRandomPhotos = function () {
-    const objectsAmount = 10;
-    const newArray = [];
-    for (let i = 0; i < window.cardcreate.cardList.length; i++) {
-      newArray.push(window.cardcreate.cardList[i]);
+    for (let i = 0; i < iterations; i++) {
+      const randomIndex = Math.floor(Math.random() * (arrayCopy.length - i)) + i;
+      const currentElement = arrayCopy[i];
+      arrayCopy[i] = arrayCopy[randomIndex];
+      arrayCopy[randomIndex] = currentElement;
     }
-    const shuffle = newArray.sort(function () {
-      return Math.random() - 0.5;
+
+    return arrayCopy;
+  };
+
+  // показываем рандомные картинки
+  const showRandomPictures = function () {
+    const picturesList = window.data.picturesList;
+    const randomElements = shuffleArray(picturesList).slice(0, MAX_RANDOM_ELEMENTS);
+
+    renderPictures(randomElements);
+  };
+
+  // обсуждаемые картинки
+  const showDiscussedPictures = function () {
+    const picturesListCopy = window.data.picturesList.slice();
+    const sortedList = picturesListCopy.sort(function (a, b) {
+      return a.comments.length - b.comments.length;
     });
 
-    shuffle.length = objectsAmount;
-    window.cardcreate.renderPictures(shuffle);
-    window.cardcreate.test(shuffle);
+    renderPictures(sortedList);
   };
-
-  // Массив фото по макс комментариям
-
-
-  const photosWithMaxComments = function () {
-    const maxCommentsPhotosArray = [];
-    for (let i = 0; i < window.cardcreate.cardList.length; i++) {
-      maxCommentsPhotosArray.push(
-          window.cardcreate.cardList[i]);
-    }
-    maxCommentsPhotosArray.sort(function (a, b) {
-      return b.comments.length - a.comments.length;
-    });
-    window.cardcreate.renderPictures(maxCommentsPhotosArray);
-    window.cardcreate.test(maxCommentsPhotosArray);
-  };
-
-
-  // Обработчики клика
-
-  const photosRemove = function () {
+  // удаление картинок
+  const removePictures = function () {
     const pics = document.querySelectorAll(`.picture`);
     pics.forEach((item) => {
       item.remove();
     });
   };
+  // функция фильтрации
+  const onFilterClick = debounce(function (evt) {
+    removePictures();
 
-
-  filterDefaultPhotos.addEventListener(`click`, function () {
-    photosRemove();
-    window.debounce(photosFromServerDefault);
+    switch (evt.target.id) {
+      case `filter-default`:
+        showDefaultPictures();
+        break;
+      case `filter-random`:
+        showRandomPictures();
+        break;
+      case `filter-discussed`:
+        showDiscussedPictures();
+        break;
+      default:
+        showDefaultPictures();
+    }
   });
-
-  filterRandomPhotos.addEventListener(`click`, function () {
-    photosRemove();
-    window.debounce(tenRandomPhotos);
-  });
-
-  filterMaxCommentsPhotos.addEventListener(`click`, function () {
-    photosRemove();
-    window.debounce(photosWithMaxComments);
-
-  });
-
-  // клик по меню
-
-  filterRandomPhotos.onclick = function () {
-    filterDefaultPhotos.classList.remove(`img-filters__button--active`);
-    filterMaxCommentsPhotos.classList.remove(`img-filters__button--active`);
-    filterRandomPhotos.classList.add(`img-filters__button--active`);
-  };
-
-  filterMaxCommentsPhotos.onclick = function () {
-    filterDefaultPhotos.classList.remove(`img-filters__button--active`);
-    filterRandomPhotos.classList.remove(`img-filters__button--active`);
-    filterMaxCommentsPhotos.classList.add(`img-filters__button--active`);
-  };
-
-  filterDefaultPhotos.onclick = function () {
-    filterRandomPhotos.classList.remove(`img-filters__button--active`);
-    filterMaxCommentsPhotos.classList.remove(`img-filters__button--active`);
-    filterDefaultPhotos.classList.add(`img-filters__button--active`);
-  };
-
+  filtersForm.addEventListener(`click`, onFilterClick);
 })();
