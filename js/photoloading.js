9854142247907c33f0b492026583a7d618fe269c
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-11-06 13:28:59 +0300

    Загрузка фото не срабатывает
    Все сделал как в демке, но фото не грузится

--- js/photoloading.js
+++ js/photoloading.js
@@ -6,10 +6,24 @@
 
   const fileChooser = document.querySelector(`.img-upload__start input[type=file]`);
   const preview = document.querySelector(`.img-upload__preview img`);
+  console.log(preview);
 
   fileChooser.addEventListener(`change`, function () {
     const file = fileChooser.files[0];
-    console.log(file)
+    const fileName = file.name.toLowerCase();
+    console.log(file);
+
+    const matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      const reader = new FileReader();
+      reader.addEventListener(`load`, function () {
+        preview.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
 
   });
 
