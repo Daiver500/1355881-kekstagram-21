1dd2bee26559302aa97a1766bae55d755e682336
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-11-06 12:27:08 +0300

    Добавляем загружаемый файл

--- bundle.js
+++ bundle.js
@@ -98,6 +98,31 @@
 })();
 
 (() => {
+/*!****************************!*\
+  !*** ./js/photoloading.js ***!
+  \****************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+
+
+(function () {
+
+  const FILE_TYPES = [`JPG`, `JPEG`, `PNG`];
+
+  const fileChooser = document.querySelector(`.img-upload__start input[type=file]`);
+  const preview = document.querySelector(`.img-upload__preview img`);
+
+  fileChooser.addEventListener(`change`, function () {
+    const file = fileChooser.files[0];
+    console.log(file)
+
+  });
+
+})();
+
+})();
+
+(() => {
 /*!**************************!*\
   !*** ./js/bigpicture.js ***!
   \**************************/
--- js/photoloading.js
+++ js/photoloading.js
@@ -1,7 +1,16 @@
 'use strict';
 
 (function () {
+
+  const FILE_TYPES = [`JPG`, `JPEG`, `PNG`];
+
   const fileChooser = document.querySelector(`.img-upload__start input[type=file]`);
   const preview = document.querySelector(`.img-upload__preview img`);
+
+  fileChooser.addEventListener(`change`, function () {
+    const file = fileChooser.files[0];
+    console.log(file)
+
+  });
 
 })();
