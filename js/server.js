ff24438f2fb66f6484c7aa148151fa6c9e4b3a05
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-05-15 22:48:47 +0300

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const REFERENCE = {
-  load: `https://21.javascript.pages.academy/kekstagram/data`,
-  upload: `https://21.javascript.pages.academy/kekstagram`
+  load: `https://javascript.pages.academy/kekstagram/data`,
+  upload: `https://javascript.pages.academy/kekstagram`
 };
 
 const TIMEOUT_IN_MS = 10000;
