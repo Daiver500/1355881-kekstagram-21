947d0ad481e405d5c27413c1d151bd8e2ca17137
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-05-20 22:22:23 +0300

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const REFERENCE = {
-  load: `https://javascript.pages.academy/kekstagram/data`,
+  load: `https://21.javascript.pages.academy/kekstagram/data`,
   upload: `https://javascript.pages.academy/kekstagram`
 };
 
